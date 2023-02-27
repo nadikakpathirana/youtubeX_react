00cf272f436e5eb96facb50d8a2ca5816a7c274d
nadikakpathirana <nadikakpathirana@gmail.com>
2023-02-27 10:06:23 +0530

    Search bar added.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import {Box} from '@mui/material';
 
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
-export const App = () => {
-  <BrowserRouter>
-    <Box sx={{ backgroudColor: '#000'}}>
+const App = () => {
+  return <BrowserRouter>
+    <Box sx={{
+        height: 100,
+        backgroundColor: '#000',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}>
       <Navbar/>
       <Routes>
         <Route path="/" exact element= {<Feed/>}/>
